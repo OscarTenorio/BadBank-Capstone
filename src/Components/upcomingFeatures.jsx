4672bd1d91db84262c4d3b5090bcbfdf43856b36
Oscar Tenorio <42981746+OscarTenorio@users.noreply.github.com>
2023-05-10 21:37:54 -0400

    Initialize Bad Bank app files
    copied files from 05/10/23, where the app is incomplete but looking to deploy the app before proceeding

---
+++ src/Components/upcomingFeatures.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Card from './card';
+
+function NewFeatures() {
+	return(
+		<>
+			<div className="d-flex justify-content-center">
+				<Card
+					width="60%"
+					margin="m-3"
+					extra="d-inline-block"
+					txtcolor="black"
+					headerbgcolor="bg-muted"
+					header="Upcoming Features in v1.0.1:"
+					text={(<>
+						<span className="d-flex justify-content-center">
+							<p>
+								&#8594; Enrollment in different account types<br></br>
+								&#8594; Test coverage using Jest<br></br>
+								&#8594; Different authentication levels for users<br></br>
+								<br></br>
+							</p>
+						</span>
+						<p className="text-center">Stay tuned for more!</p>
+					</>)}
+				/>
+			</div>
+		</>
+	);
+}
+
+export default NewFeatures;
---
+++ src/Components/userContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserContext = 
+	React.createContext({
+		user: {},
+		setUser: () => {}
+	});
+
+export default UserContext;
